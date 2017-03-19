03399f46915fa5647ea2d5747c924a5133a2a983
majovec <jakub.mai97@gmail.com>
2017-03-19 15:08:24 +0100

    Add files via upload

---
+++ src/jl/JumpLeague/Tasks/JLResetSignsTask.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace jl\JumpLeague\Tasks;
+
+use jl\JumpLeague\Game\ArenaManager;
+use jl\JumpLeague\Main\JumpLeague;
+use pocketmine\scheduler\PluginTask;
+
+class JLResetSignsTask extends PluginTask
+{
+
+    public function __construct(JumpLeague $owner)
+    {
+        parent::__construct($owner);
+    }
+
+    public function onRun($currentTick)
+    {
+        ArenaManager::resetSigns();
+    }
+
+}
